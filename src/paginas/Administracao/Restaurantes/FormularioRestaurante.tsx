89d024dc5817cff5247d6cca117a9898cf77d97b
Antonio Vieira <vieira.n55@gmail.com>
2022-10-14 17:19:59 -0300

    Listas de Busca ok

---
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,61 @@
+import { Box, Button, TextField, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import http from "../../../http"
+import IRestaurante from "../../../interfaces/IRestaurante"
+
+const FormularioRestaurante = () => {
+
+  const parametros = useParams()
+
+  useEffect(() => {
+    if (parametros.id) {
+      http.get<IRestaurante>(`restaurantes/${parametros.id}/`)
+        .then(resposta => setNomeRestaurante(resposta.data.nome))
+    }
+  }, [parametros])
+
+  const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+  const aoSubmiterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+
+    if (parametros.id) {
+      http.put(`restaurantes/${parametros.id}/`, {
+        nome: nomeRestaurante
+      })
+        .then(() => {
+          alert("Restaurante atualizado com sucesso")
+        })
+    } else {
+      http.post("restaurantes/", {
+        nome: nomeRestaurante
+      })
+        .then(() => {
+          alert("Restaurante Cadastrado com sucesso")
+        })
+    }
+  }
+
+  return (
+    <Box sx={{display: 'flex', flexDirection: "Column", alignItems: "center"}}>
+      <Typography component="h1" variant="h6">
+        Formulário de Restaurantes
+      </Typography>
+      <Box component="form" onSubmit={aoSubmiterForm}>
+        <TextField 
+          value={nomeRestaurante}
+          onChange={evento => setNomeRestaurante(evento.target.value)}
+          id="standard-basic"
+          label="Nome do Restaurante"
+          variant="standard" 
+          fullWidth
+          required
+          />
+        <Button sx={{  marginTop: 1}} type="submit" fullWidth variant="outlined">Salvar</Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default FormularioRestaurante
